0cb83a1a568bff8ad7d924adaa39bb400389f0b2
EyeFox <pentesting833@gmail.com>
2026-02-18 13:46:18 +0800

    Add files via upload

--- setup2.py
+++ setup2.py
@@ -443,7 +443,7 @@ def extract_and_setup(zip_path):
     with open(config_path, 'r') as f:
         config = json.load(f)
     
-    icon_path = os.path.join(temp_dir, "icon.png")
+    icon_path = os.path.join(temp_dir, "icon.webp")
     if not os.path.exists(icon_path):
         icon_path = None
     
